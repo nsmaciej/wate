943d002bc83bc7176f27ac627442257ddcfc78b0
Maciej Goszczycki <mgoszcz2@gmail.com>
2022-01-26 17:39:59 +0100

    Fix whitespace in the help modal

--- src/lib/modals/Help.svelte
+++ src/lib/modals/Help.svelte
@@ -28,8 +28,8 @@
     <a href="https://maciej.ie">Maciej Goszczycki</a>.
   </p>
   <p>
-    sitelen pona typeset in
-    <a href="https://musilili.net/linja-pona/">linja pona</a>.
+    Sitelen Pona typeset in
+    <a href="https://musilili.net/linja-pona/">Linja Pona</a>.
   </p>
   <p>
     Check out the code on
@@ -41,26 +41,26 @@
       "Wordle",
       "[_waso_alasa_toki_esun]"
     )}.
-    <a href="https://www.powerlanguage.co.uk/wordle/" title="Wordle">
-      o musi e musi nanpa wan
-    </a>.
+    <a href="https://www.powerlanguage.co.uk/wordle/" title="Wordle"
+      >o musi e musi nanpa wan</a
+    >.
   </p>
   <p>
     lipu ni tan
-    <a href="https://maciej.ie" title="Maciej Goszczycki">
-      {$sitelen("jan Masije", "jan [_mun_ante_sona_ilo_jan_esun]")}
-    </a>.
+    <a href="https://maciej.ie" title="Maciej Goszczycki"
+      >{$sitelen("jan Masije", "jan [_mun_ante_sona_ilo_jan_esun]")}</a
+    >.
   </p>
   <p>
     {$combine("sitelen pona")} kepeken
-    <a href="https://musilili.net/linja-pona/" title="linja pona">
-      {$combine("linja pona")}
-    </a>.
+    <a href="https://musilili.net/linja-pona/" title="linja pona"
+      >{$combine("linja pona")}</a
+    >.
   </p>
   <p>
     o lukin e ilo kon ni lon
-    <a href="https://github.com/mgoszcz2/wate" title="GitHub">
-      {$sitelen("lipu GitHub", "lipu [_kulupu_ilo_toki_awen_pona]")}
-    </a>.
+    <a href="https://github.com/mgoszcz2/wate" title="GitHub"
+      >{$sitelen("lipu GitHub", "lipu [_kulupu_ilo_toki_awen_pona]")}</a
+    >.
   </p>
 {/if}
