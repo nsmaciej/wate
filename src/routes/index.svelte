afb5704ce1967bab09e0bfe39e5bf7077dd3dcb0
Maciej Goszczycki <mgoszcz2@gmail.com>
2022-02-16 19:36:56 +0100

    Wordy OG metadata and share image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,124 +1,34 @@
 <script context="module" lang="ts">
-  import dictionaryTok from "$static/dictionary-tok.json";
-  import dictionaryEn from "$static/dictionary-en.json";
-  import { wordy } from "$static/config.json";
-  const dictionary = wordy ? dictionaryEn : dictionaryTok;
-
+  import App from "$lib/App.svelte";
   import { waitLocale } from "svelte-i18n";
+  import { wordy } from "$static/config.json";
   import "./i18n";
   import "../app.css";
 
+  const assetSuffix = wordy ? "en" : "tok";
+  const title = wordy ? "Wordy" : "Wate";
+  const domain = wordy ? "https://wordy.maciej.ie" : "https://wate.maciej.ie";
+  const description = wordy
+    ? "Open Source Wordle"
+    : "Toki Pona Wordle. musi Wordle pi toki pona.";
   export async function preload(): Promise<void> {
     return waitLocale();
   }
 </script>
 
-<script lang="ts">
-  import {
-    currentGameDay,
-    selectEnglishWord,
-    selectTokiPonaWord,
-  } from "$lib/game";
-  import {
-    DNF_STATS_KEY,
-    finishedStats,
-    gameState,
-    gatherSettings,
-    tokiPonaMode,
-  } from "$lib/settings";
-  import { _ } from "svelte-i18n";
-  import { localStorageStore } from "$lib/utils";
-  import { recordEvent } from "$lib/countClick";
-  import Header from "$lib/Header.svelte";
-  import Modal from "$lib/Modal.svelte";
-  import Toasts, { showToast } from "$lib/Toasts.svelte";
-  import Wordle from "$lib/Wordle.svelte";
-  import Share from "$lib/modals/Share.svelte";
-  import Backdoor from "$lib/Backdoor.svelte";
-
-  const acceptableWords = dictionary.solutions.concat(dictionary.words);
-  const gameDay = currentGameDay();
-  const lastDayPlayed = localStorageStore("day", gameDay);
-  if (gameDay !== $lastDayPlayed) gameState.reset();
-  $lastDayPlayed = gameDay;
-
-  $: solution = wordy
-    ? selectEnglishWord(dictionary.solutions, gameDay)
-    : selectTokiPonaWord(dictionary.solutions, $tokiPonaMode, gameDay);
-  let shareModalShown = false;
-  let showingHeaderModal = false;
-
-  function recordGameFinish(key: number) {
-    const old = $finishedStats[$tokiPonaMode][key] ?? 0;
-    $finishedStats[$tokiPonaMode][key] = old + 1;
-    // Acceptable data collection. General settings plus the board.
-    recordEvent(
-      key === DNF_STATS_KEY ? "dnf" : "finish",
-      ...gatherSettings(),
-      $gameState[$tokiPonaMode].join(",")
-    );
-  }
-
-  async function handleWin() {
-    const message = [
-      $_("toast.won-in-one"),
-      $_("toast.won-in-two"),
-      $_("toast.won-in-three"),
-      $_("toast.won-in-four"),
-      $_("toast.won-in-five"),
-    ];
-    const rowsUsed = $gameState[$tokiPonaMode].length;
-    await showToast(message[rowsUsed - 1], 0.5);
-    recordGameFinish(rowsUsed);
-    showShareModal();
-  }
-
-  async function handleLost() {
-    await showToast(solution);
-    recordGameFinish(DNF_STATS_KEY);
-    showShareModal();
-  }
-
-  function showShareModal() {
-    if (!showingHeaderModal) {
-      shareModalShown = true;
-    }
-  }
-</script>
-
 <svelte:head>
-  <title>{$_("name-title")}</title>
+  <title>{title}</title>
+
+  <link rel="apple-touch-icon" href="/apple-touch-icon-{assetSuffix}.png" />
+  <link rel="icon" sizes="192x192" href="/icon-{assetSuffix}.png" />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="{domain}/share-{assetSuffix}.png" />
+  <meta property="og:url" content={domain} />
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+  <meta name="description" content={description} />
 </svelte:head>
 
-<main style:--app-width="{wordy ? 500 : 360}px">
-  <Header bind:showingModal={showingHeaderModal} />
-  {#key solution}
-    <Wordle
-      dictionary={acceptableWords}
-      {solution}
-      on:complete={showShareModal}
-      on:win={handleWin}
-      on:lost={handleLost}
-      bind:submittedRows={$gameState[$tokiPonaMode]}
-    />
-  {/key}
-  <Toasts />
-  <Modal title={$_("modal.share")} bind:shown={shareModalShown}>
-    <Share {gameDay} {solution} />
-  </Modal>
-  <Backdoor />
-</main>
-
-<style>
-  main {
-    display: flex;
-    flex-flow: column;
-    align-items: center;
-    justify-content: space-around;
-    max-width: var(--app-width);
-    min-height: 100%;
-    margin: 0 auto;
-    padding: 10px;
-    gap: 8px;
-  }
-</style>
+<App />
